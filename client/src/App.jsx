22969b2be782616d0252a0fbbf9064539b0e68fa
apoorvvikram01 <135138854+apoorvvikram01@users.noreply.github.com>
2024-09-18 14:43:15 +0530

    routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Projects from './pages/Projects.jsx'
+import About from './pages/About.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
 function App() {
   return (
-    <div className='text-3xl'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/projects' element={<Projects/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/*' element={<PageNotFound/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
