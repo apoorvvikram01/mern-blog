5b7ef063596cc212aea9513ca202fcb8cd1cc3d4
apoorvvikram01 <135138854+apoorvvikram01@users.noreply.github.com>
2024-09-19 18:24:13 +0530

    signIn controller completed

---
+++ api/controllers/signUp.controller.js
@@ -0,0 +1,44 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+import errorHandler from "../utils/error.js"
+
+
+
+export const signup = async (req, res,next) => {
+  const { username, password, email } = req.body;
+
+  if (
+    !username ||
+    !password ||
+    !email ||
+    username === "" ||
+    password === "" ||
+    email === ""
+  ) {
+    next (errorHandler(400,"All fields are required"))
+  }
+
+// Hashing  password
+const hashedPassword = bcryptjs.hashSync(password,10)
+
+
+// Saving in new user
+  const newUser = new User({ username,
+    password:hashedPassword,
+     email });
+
+  try {
+
+    await newUser.save();
+
+    res.status(200).json({
+      message: "Signed  up successfully",
+      success: true,
+    });
+  } catch (error) {
+   next(error);
+   
+  }
+};
+
+
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup,signin } from '../controllers/auth.controller.js';
+import { signup } from '../controllers/signUp.controller.js';
+import { signin } from '../controllers/signIn.controller.js';
 
 const router = express.Router();
 
-router.post('/signup',signup)
-router.post('/signin',signin)
+router.post('/signup',signup);
+router.post('/signin',signin);
 
-export default signup;
+export default router;
