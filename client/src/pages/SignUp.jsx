45e3468a4d44ef3b3b01b13dac5826ea506ad0a5
apoorvvikram01 <135138854+apoorvvikram01@users.noreply.github.com>
2024-09-19 14:16:43 +0530

    signup functionality added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,46 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React,{useState} from "react";
+import { NavLink,useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc"; // Importing Google icon
+import { Alert, Spinner } from "flowbite-react";
+import axios from "axios";
+
 
 const SignUp = () => {
+  const [formData,setFormData] = useState({});
+  const [errorMessage,setErrorMessage] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+   setFormData({...formData ,[e.target.id]:e.target.value.trim()});
+  }
+   const handleSubmit = async (e)=>{
+    e.preventDefault();
+    if(!formData.username || !formData.password || !formData.email ){
+      return setErrorMessage('Please fill all the fields')
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await axios.post('/api/auth/sign-up',{
+        method: 'POST',
+        headers:{'Content-type': 'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if (data.success===false) {
+        return setErrorMessage(data.message) 
+      }
+      setLoading(false);
+      if(res.ok){
+        res.navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+   }
+  
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       {/* Left side of the signup page (hidden on small screens) */}
@@ -25,7 +63,7 @@ const SignUp = () => {
           </h2>
 
           {/* Form starts here */}
-          <form className="w-full">
+          <form className="w-full" onSubmit={handleSubmit}>
             {/* Username */}
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -36,6 +74,7 @@ const SignUp = () => {
                 type="text"
                 className="w-full px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
                 placeholder="Username"
+                onChange={handleChange}
               />
             </div>
 
@@ -49,6 +88,7 @@ const SignUp = () => {
                 type="email"
                 className="w-full px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
                 placeholder="name@example.com"
+                onChange={handleChange}
               />
             </div>
 
@@ -62,6 +102,7 @@ const SignUp = () => {
                 type="password"
                 className="w-full px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
                 placeholder="Password"
+                onChange={handleChange}
               />
             </div>
 
@@ -76,10 +117,17 @@ const SignUp = () => {
 
             {/* Submit Button */}
             <button
+            disabled={loading}
               type="submit"
               className="w-full px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 font-semibold rounded-md focus:outline-none"
             >
-              Sign Up
+             {loading? (
+              <>
+              <Spinner size='sm' />
+              <span className="pl-3">Loading...</span>
+              </>
+             ) : "Sign Up "
+            }
             </button>
           </form>
           {/* Form ends here */}
@@ -96,7 +144,15 @@ const SignUp = () => {
           </p>
         </div>
       </div>
+      {
+      errorMessage &&(
+        <Alert className="mt-5" color='failure'>
+        {errorMessage}
+        </Alert>
+      )
+    }
     </div>
+ 
   );
 };
 
